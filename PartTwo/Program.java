809fd9cc2f2f4d407026a44a163598d86710275c
Mario Fernandez <diddleslip@gmail.com>
2023-12-12 13:49:46 -0500

    Making progress on PartTwo of java tutorial

--- PartTwo/Program.java
+++ PartTwo/Program.java
@@ -5,23 +5,54 @@ import java.util.Scanner;
 public class Program {
   public static void main(String[] main) {
     // Creating the scanner
-    Scanner scanner = new Scanner(System.in);
+    Scanner reader = new Scanner(System.in);
 
     // Example of reading different types of user input
     // String text = reader.nextLine();
     // int number = Integer.valueOf(reader.nextLine());
     // double numberWithDecimals = Double.valueOf(reader.nextLine());
-    // boolean trueOrFalse = Boolean.valueOf(reader.nextLine());
+    // // boolean trueOrFalse = Boolean.valueOf(reader.nextLine());
+    // int first = 1;
+    // int second = 2;
+    // int sum = first + second;
 
-    try {
-      // Example of reading different types of user input
-      int number = Integer.valueOf(scanner.nextLine());
-      int result = number * 2;
-      System.out.println(result);
-    } finally {
-      // Closing the scanner in a finally block
-      scanner.close();
+    // System.out.println("The sum of " + first + " and " +
+    //  second + " is " + sum);
+
+    // // Assigning the user input to variables
+    // int first = Integer.valueOf(reader.nextLine());
+    // int second = Integer.valueOf(reader.nextLine());
+
+    // // Identifying the operation and declaring a variable for the result
+    // int product = first * second;
+
+    // // Printing the result of the operation
+    // System.out.println("The product of " + first +
+    // " and " + second + " is " + product);
+
+    // int first = Integer.valueOf(reader.nextLine());
+    // int second = Integer.valueOf(reader.nextLine());
+    // double squareRoot = Math.sqrt(first+second);
+
+    // System.out.println(squareRoot);
+
+    // int first = Integer.valueOf(reader.nextLine());
+    // int second = Integer.valueOf(reader.nextLine());
+
+    // if (first + second > 100) {
+    //   System.out.println("too much");
+    // } else if (first + second < 100) {
+    //   System.out.println("too little");
+    // } else {
+    //   System.out.println("ok");
+    // }
+
+    int first = Integer.valueOf(reader.nextLine());
+
+    if (first < 0) {
+      System.out.println(first * -1);
+    } else {
+      System.out.println(first);
     }
-
   }
 }
