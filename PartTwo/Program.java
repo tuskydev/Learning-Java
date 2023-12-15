806111e25fd2b1c6b17740dd98d2fd9711d89cc5
? <test>
2023-12-15 12:31:44 +0000

    New day lets goo!

--- PartTwo/Program.java
+++ PartTwo/Program.java
@@ -3,85 +3,87 @@ package PartTwo;
 import java.util.Scanner;
 
 public class Program {
-  public static void main(String[] args) {
-    // Creating the scanner
-    Scanner reader = new Scanner(System.in);
+    public static void main(String[] main) {
+        // Creating the scanner
+        Scanner reader = new Scanner(System.in);
 
-    // Example of reading different types of user input
-    // String text = reader.nextLine();
-    // int number = Integer.valueOf(reader.nextLine());
-    // double numberWithDecimals = Double.valueOf(reader.nextLine());
-    // // boolean trueOrFalse = Boolean.valueOf(reader.nextLine());
-    // int first = 1;
-    // int second = 2;
-    // int sum = first + second;
+        // Example of reading different types of user input
+        // String text = reader.nextLine();
+        // int number = Integer.valueOf(reader.nextLine());
+        // double numberWithDecimals = Double.valueOf(reader.nextLine());
+        // // boolean trueOrFalse = Boolean.valueOf(reader.nextLine());
+        // int first = 1;
+        // int second = 2;
+        // int sum = first + second;
 
-    // System.out.println("The sum of " + first + " and " +
-    //  second + " is " + sum);
+        // System.out.println("The sum of " + first + " and " +
+        //  second + " is " + sum);
 
-    // // Assigning the user input to variables
-    // int first = Integer.valueOf(reader.nextLine());
-    // int second = Integer.valueOf(reader.nextLine());
+        // // Assigning the user input to variables
+        // int first = Integer.valueOf(reader.nextLine());
+        // int second = Integer.valueOf(reader.nextLine());
 
-    // // Identifying the operation and declaring a variable for the result
-    // int product = first * second;
+        // // Identifying the operation and declaring a variable for the result
+        // int product = first * second;
 
-    // // Printing the result of the operation
-    // System.out.println("The product of " + first +
-    // " and " + second + " is " + product);
+        // // Printing the result of the operation
+        // System.out.println("The product of " + first +
+        // " and " + second + " is " + product);
 
-    // int first = Integer.valueOf(reader.nextLine());
-    // int second = Integer.valueOf(reader.nextLine());
-    // double squareRoot = Math.sqrt(first+second);
+        // int first = Integer.valueOf(reader.nextLine());
+        // int second = Integer.valueOf(reader.nextLine());
+        // double squareRoot = Math.sqrt(first+second);
 
-    // System.out.println(squareRoot);
+        // System.out.println(squareRoot);
 
-    // int first = Integer.valueOf(reader.nextLine());
-    // int second = Integer.valueOf(reader.nextLine());
+        // int first = Integer.valueOf(reader.nextLine());
+        // int second = Integer.valueOf(reader.nextLine());
 
-    // if (first + second > 100) {
-    //   System.out.println("too much");
-    // } else if (first + second < 100) {
-    //   System.out.println("too little");
-    // } else {
-    //   System.out.println("ok");
-    // }
+        // if (first + second > 100) {
+        //   System.out.println("too much");
+        // } else if (first + second < 100) {
+        //   System.out.println("too little");
+        // } else {
+        //   System.out.println("ok");
+        // }
 
-    // int first = Integer.valueOf(reader.nextLine());
+        // int first = Integer.valueOf(reader.nextLine());
 
-    // if (first < 0) {
-    //   System.out.println(first * -1);
-    // } else {
-    //   System.out.println(first);
-    // }
+        // if (first < 0) {
+        //   System.out.println(first * -1);
+        // } else {
+        //   System.out.println(first);
+        // }
 
-    // int numOne = Integer.valueOf(reader.nextLine());
-    // int numTwo = Integer.valueOf(reader.nextLine());
+        // int numOne = Integer.valueOf(reader.nextLine());
+        // int numTwo = Integer.valueOf(reader.nextLine());
 
-    // if (numOne > numTwo) {
-    //   System.out.println(numOne + " is greater than " + numTwo + ".");
-    // } else if (numOne < numTwo) {
-    //   System.out.println(numOne + " is smaller than " + numTwo + ".");
-    // } else {
-    //   System.out.println(numOne + " is equal to " + numTwo + ".");
-    // }
+        // if (numOne > numTwo) {
+        //   System.out.println(numOne + " is greater than " + numTwo + ".");
+        // } else if (numOne < numTwo) {
+        //   System.out.println(numOne + " is smaller than " + numTwo + ".");
+        // } else {
+        //   System.out.println(numOne + " is equal to " + numTwo + ".");
+        // }
 
-    // int numbersRead = 0;
-    // int sum = 0;
+        // int numbersRead = 0;
+        // int sum = 0;
 
-    // while (true) {
-    //   if (numbersRead == 5) {
-    //     break;
-    //   }
+        // while (true) {
+        //   if (numbersRead == 5) {
+        //     break;
+        //   }
 
-    //   System.out.println("Input number");
-    //   sum = sum + Integer.valueOf(reader.nextLine());
-    //   numbersRead += 1;
-    // }
+        //   System.out.println("Input number");
+        //   sum = sum + Integer.valueOf(reader.nextLine());
+        //   numbersRead += 1;
+        // }
 
-    // System.out.println("The sum of the numbers is " + sum);
+        // System.out.println("The sum of the numbers is " + sum);
 
-    while (true) {
+
+        /*/
+      while (true) {
         System.out.println("Shall we carry on?");
 
         String input = reader.nextLine();
@@ -89,7 +91,21 @@ public class Program {
         if (input.equals("no")) {
             break;
         }
+      /*/
+
+        int iteration = 0;
+        while (true) {
+            System.out.println("Give a number: ");
+            int input = Integer.valueOf(reader.nextLine());            
+            
+            if (input == 0 ) {
+                break;
+            }
+
+            iteration += 1;
+        }
+
+        System.out.println("Number of numbers: " + iteration);
 
     }
-  }
 }
